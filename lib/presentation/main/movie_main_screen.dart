e2556ad9b83f092fd85ac8b0adac687a89fc687f
sungchanjung <wjdtjdcks31@gmail.com>
2024-04-19 20:27:46 +0900

    feat: movie_archived_screen UI, viewModel added

--- lib/presentation/main/movie_main_screen.dart
+++ lib/presentation/main/movie_main_screen.dart
@@ -3,6 +3,11 @@ import 'package:movie_project/presentation/archived/movie_archived_screen.dart';
 import 'package:movie_project/presentation/main/movie_main_view_model.dart';
 import 'package:provider/provider.dart';
 
+import '../../data/data_source/movie_data_source.dart';
+import '../../data/repository/movie_repository_impl.dart';
+import '../../main.dart';
+import '../archived/movie_archived_view_model.dart';
+
 class MovieMainScreen extends StatefulWidget {
   const MovieMainScreen({super.key});
 
@@ -27,12 +32,14 @@ class _MovieMainScreenState extends State<MovieMainScreen> {
       floatingActionButton: Row(
         children: [
           FloatingActionButton(
+            heroTag: '넘길래요',
             onPressed: () {
               viewModel.onPass();
             },
             child: Text('넘길래요'),
           ),
           FloatingActionButton(
+            heroTag: '좋아요',
             onPressed: () {
               viewModel.onLikes(viewModel.movieList[viewModel.index]);
             },
@@ -47,8 +54,14 @@ class _MovieMainScreenState extends State<MovieMainScreen> {
             onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (BuildContext context) => MovieArchivedScreen(
-
+                  builder: (context) => ChangeNotifierProvider(
+                    create: (_) => MovieArchivedViewModel(
+                      movieRepository: MovieRepositoryImpl(
+                        api: MovieDataSource(),
+                        archived: archived,
+                      ),
+                    ),
+                    child: MovieArchivedScreen(),
                   ),
                 ),
               );
