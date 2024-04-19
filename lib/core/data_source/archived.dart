ee092a09efd400dbbbb224055d85e70dee323828
sseung <sseutta@naver.com>
2024-04-19 18:19:47 +0900

    feat: likeList 생성, Provider 추가, viewModel 메서드 추가 .

---
+++ lib/core/data_source/archived.dart
@@ -0,0 +1,9 @@
+import 'package:movie_project/domain/model/movie.dart';
+
+class Archived {
+  List<Movie> likeList = [];
+
+  Archived(
+    this.likeList,
+  );
+}
--- lib/presentation/main/movie_main_view_model.dart
+++ lib/presentation/main/movie_main_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_project/domain/model/movie.dart';
 import 'package:movie_project/domain/repository/movie_repository.dart';
+import 'package:movie_project/main.dart';
 
 class MovieMainViewModel with ChangeNotifier {
   final MovieRepository _movieRepository;
@@ -10,24 +11,40 @@ class MovieMainViewModel with ChangeNotifier {
   }) : _movieRepository = movieRepository;
 
   List<Movie> _movieList = [];
+
   List<Movie> get movieList => List.unmodifiable(_movieList);
 
-  List<Movie> _likeList = [];
-  List<Movie> get likeList => List.unmodifiable(_likeList);
+  // List<Movie> _likeList = [];
+  //
+  // List<Movie> get likeList => List.unmodifiable(_likeList);
 
-  bool _isLoading = false;
+  bool _isLoading = true;
+
   bool get isLoading => _isLoading;
 
+  int _index = 0;
+
+  int get index => _index;
+
   void showMovie() async {
+    _isLoading = true;
+    notifyListeners();
+
     final data = await _movieRepository.getMovieList();
     _movieList = data;
+    _isLoading = false;
     notifyListeners();
   }
 
   void onLikes(Movie movie) async {
-    // _likeList = 현재 인덱스 무비를 담는다
-    // 다음게 나온다
+    archived.likeList.add(movie);
+    print(archived.likeList);
+    _index++;
+    notifyListeners();
   }
 
-  void onPass() async {}
+  void onPass() async {
+    _index++;
+    notifyListeners();
+  }
 }
