a2cdb3e15eeb5bee154b37bdbfe2e48de880d80d
sungchanjung <wjdtjdcks31@gmail.com>
2024-04-19 12:51:02 +0900

    feat: create model, dto, api

---
+++ lib/data/data_source/movie_data_source.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../dto/movie_dto.dart';
+
+class MovieDataSource {
+  final _baseUrl = "https://api.themoviedb.org/3/movie/popular?api_key=";
+  final _apiKey = '84903395cbf9b0e42926167ee3526acb';
+
+  Future<List<MovieDto>> getMovie() async {
+    final response =
+        await http.get(Uri.parse('$_baseUrl$_apiKey&language=ko-KR'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      final List<dynamic> results = jsonData['results'];
+      return results.map((json) => MovieDto.fromJson(json)).toList();
+    } else {
+      throw Exception('Error');
+    }
+  }
+}
