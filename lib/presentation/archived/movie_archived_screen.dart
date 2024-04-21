7d18c7aea09a8462cb2cbdedb0905691bfa3c6f8
sungchanjung <wjdtjdcks31@gmail.com>
2024-04-21 16:56:10 +0900

    modify: UI 수정

--- lib/presentation/archived/movie_archived_screen.dart
+++ lib/presentation/archived/movie_archived_screen.dart
@@ -19,112 +19,98 @@ class _MovieArchivedScreenState extends State<MovieArchivedScreen> {
       appBar: AppBar(
         title: Text('나의 보관함'),
       ),
-      body: SingleChildScrollView(
-        scrollDirection: Axis.vertical,
-        child: Column(
-          children: [
-            SizedBox(
-              height: 24,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height - 96,
-              child: ListView(
-                children: archived.likeList.map((e) {
-                  return Container(
-                    height: 144,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                          top: 0, left: 16.0, right: 16.0, bottom: 16),
-                      child: Row(
-                        children: [
-                          ClipRRect(
-                            borderRadius: BorderRadius.circular(8),
-                            child: Image.network(
-                              "https://image.tmdb.org/t/p/w500/${e.posterPath}",
-                              width: 144,
-                              height: 144,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 8,
-                          ),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
+      body: ListView(
+        children: archived.likeList.map((e) {
+          return Container(
+            margin: EdgeInsets.only(top: e == archived.likeList.first? 16 : 0, left: 0, right: 0, bottom: e == archived.likeList.last? 16 : 0),
+            height: 150,
+            child: Padding(
+              padding: EdgeInsets.only( top: 8, left: 16.0, right: 16.0, bottom: 8),
+              child: Row(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: Image.network(
+                      "https://image.tmdb.org/t/p/w500/${e.posterPath}",
+                      width: 144,
+                      height: 144,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
                               children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Icon(
-                                          Icons.star,
-                                          color: Color(0xFFFFB800),
-                                          size: 16,
-                                        ),
-                                        SizedBox(
-                                          width: 4,
-                                        ),
-                                        Text(
-                                          ((e.voteAverage * 10).round() / 10)
-                                              .toString(),
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(
-                                      width: 18,
-                                      height: 18,
-                                      child: IconButton(
-                                        onPressed: () {
-                                          viewModel.onDelete(e.id);
-                                        },
-                                        padding: EdgeInsets.zero,
-                                        icon: Icon(
-                                          Icons.delete,
-                                          size: 18,
-                                          color: Colors.red.shade400,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
+                                Icon(
+                                  Icons.star,
+                                  color: Color(0xFFFFB800),
+                                  size: 16,
+                                ),
+                                SizedBox(
+                                  width: 4,
                                 ),
                                 Text(
-                                  e.title,
-                                  maxLines: 1, overflow: TextOverflow.ellipsis,
+                                  ((e.voteAverage * 10).round() / 10)
+                                      .toString(),
                                   style: TextStyle(
                                       fontSize: 16,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                                SizedBox(
-                                  height: 4,
-                                ),
-                                SizedBox(
-                                  width: MediaQuery.of(context).size.width,
-                                  height: 60,
-                                  child: Text(
-                                    e.overview,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 3,
-                                  ),
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
+                            SizedBox(
+                              width: 18,
+                              height: 18,
+                              child: IconButton(
+                                onPressed: () {
+                                  viewModel.onDelete(e.id);
+                                },
+                                padding: EdgeInsets.zero,
+                                icon: Icon(
+                                  Icons.delete,
+                                  size: 18,
+                                  color: Colors.red.shade400,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          e.title,
+                          maxLines: 1, overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          height: 60,
+                          child: Text(
+                            e.overview,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 3,
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  );
-                }).toList(),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          );
+        }).toList(),
       ),
     );
   }
