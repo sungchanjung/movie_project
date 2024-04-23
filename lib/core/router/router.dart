05920b8284e71e2d73f782940a581570b9e76fee
Yejin.Shin <mingmi_27@naver.com>
2024-04-23 21:13:18 +0900

    feat: add di setup

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,8 +1,5 @@
 import 'package:go_router/go_router.dart';
-import 'package:movie_project/data/data_source/movie_data_source.dart';
-import 'package:movie_project/data/repository/movie_repository_impl.dart';
 import 'package:movie_project/di/di_setup.dart';
-import 'package:movie_project/main.dart';
 import 'package:movie_project/presentation/archived/movie_archived_screen.dart';
 import 'package:movie_project/presentation/archived/movie_archived_view_model.dart';
 import 'package:movie_project/presentation/main/movie_main_screen.dart';
@@ -23,9 +20,7 @@ final router = GoRouter(routes: [
     path: '/archived',
     builder: (context, state) {
       return ChangeNotifierProvider(
-        create: (context) => MovieArchivedViewModel(
-            movieRepository: MovieRepositoryImpl(
-                api: MovieDataSource(), archived: archived)),
+        create: (context) => getIt<MovieArchivedViewModel>(),
         child: const MovieArchivedScreen(),
       );
     },
