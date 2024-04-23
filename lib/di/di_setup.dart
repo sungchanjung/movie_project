d335c7f84f37b26fbed56c14620d42cf3d1ca99b
Yejin.Shin <mingmi_27@naver.com>
2024-04-23 22:22:25 +0900

    feat: modifying Future<void>

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,4 +1,5 @@
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:movie_project/core/data_source/archived.dart';
 import 'package:movie_project/data/data_source/movie_data_source.dart';
 import 'package:movie_project/data/repository/movie_repository_impl.dart';
@@ -7,11 +8,16 @@ import 'package:movie_project/presentation/archived/movie_archived_view_model.da
 import 'package:movie_project/presentation/main/movie_main_view_model.dart';
 
 final getIt = GetIt.instance;
-void diSetup() {
+Future<void> diSetup() async {
   getIt.registerSingleton<Archived>(Archived([]));
+
+  var hiveBox = await Hive.openBox<String>('archivedList.db');
+  getIt.registerSingleton<Box<String>>(hiveBox);
+
   getIt.registerSingleton<MovieDataSource>(MovieDataSource());
   getIt.registerSingleton<MovieRepository>(
       MovieRepositoryImpl(api: getIt(), archived: getIt()));
+
   getIt.registerFactory(() => MovieMainViewModel(movieRepository: getIt()));
   getIt.registerFactory(() => MovieArchivedViewModel(movieRepository: getIt()));
 }
