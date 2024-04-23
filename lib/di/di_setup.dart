4ac2fbb46218f1a44a97042e9fd40deca51bd67c
Yejin.Shin <mingmi_27@naver.com>
2024-04-23 21:13:03 +0900

    feat: di setup

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -3,6 +3,7 @@ import 'package:movie_project/core/data_source/archived.dart';
 import 'package:movie_project/data/data_source/movie_data_source.dart';
 import 'package:movie_project/data/repository/movie_repository_impl.dart';
 import 'package:movie_project/domain/repository/movie_repository.dart';
+import 'package:movie_project/presentation/archived/movie_archived_view_model.dart';
 import 'package:movie_project/presentation/main/movie_main_view_model.dart';
 
 final getIt = GetIt.instance;
@@ -12,4 +13,5 @@ void diSetup() {
   getIt.registerSingleton<MovieRepository>(
       MovieRepositoryImpl(api: getIt(), archived: getIt()));
   getIt.registerFactory(() => MovieMainViewModel(movieRepository: getIt()));
+  getIt.registerFactory(() => MovieArchivedViewModel(movieRepository: getIt()));
 }
